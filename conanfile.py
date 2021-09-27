6c6e18cf8bea52d0d4b4132f6b52b4754c0d27cf
MichelleHetzel <77071503+MichelleHetzel@users.noreply.github.com>
2021-09-27 13:47:39 -0500

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,27 +1,27 @@
 from conans import ConanFile
 
 class Pkg(ConanFile):
-   python_requires = "pyreq/0.1@user/channel"  # recipe to reuse code from
-   build_requires = "tool_a/0.2@user/testing", "tool_b/0.2@user/testing"
-   requires = "req_a/1.0", "req_l/2.1@otheruser/testing"
+   python_requires = "pybind11/0.1"  # recipe to reuse code from
+   build_requires = "toml/0.2@user/testing", "libsass/0.2@user/testing"
+   requires = "tl-function-ref/1.0", "zulu-openjdk/2.1@otheruser/testing"
 
-   requires = [("req_b/0.1@user/testing"),
-               ("req_d/0.2@dummy/stable", "override"),
-               ("req_e/2.1@coder/beta", "private")]
+   requires = [("optional-lite/0.1@user/testing"),
+               ("snappy/0.2@dummy/stable", "override"),
+               ("wayland/2.1@coder/beta", "private")]
 
-   requires = (("req_c/1.0@user/stable", "private"), )
-   requires = ("req_f/1.0@user/stable", ("req_h/3.0@other/beta", "override"))
-   requires = "req_g/[>1.0 <1.8]@user/stable"
+   requires = (("libuv/1.0@user/stable", "private"), )
+   requires = ('ninja/1.0@user/stable', ("tinyspline/3.0@other/beta", "override"))
+   requires = "c-ares/[>1.0 <1.8]@user/stable"
 
 
 def requirements(self):
    if self.options.myoption:
-      self.requires("req_i/1.2@drl/testing")
+      self.requires('c-blosc/1.2@drl/testing')
    else:
-      self.requires("req_i/2.2@drl/stable")
-      self.requires("req_k/1.2@drl/testing", private=True, override=False)
+      self.requires("c-blosc/2.2@drl/stable")
+      self.requires("dbus/1.2@drl/testing", private=True, override=False)
 
 
 def build_requirements(self):
    if self.settings.os == "Windows":
-      self.build_requires("tool_win/0.1@user/stable")
+      self.build_requires("faketool/0.1@user/stable")
